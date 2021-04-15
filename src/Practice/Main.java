8137d1b3f70b0843dca0cf96f98ab50bb0e29132
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2021-04-15 13:34:34 +0900

    Project has been updated

--- src/Practice/Main.java
+++ src/Practice/Main.java
@@ -3,21 +3,6 @@ package Practice;
 
 public class Main {
     public static  void main(String[] args){
-        /** Main method */
-            // Declare and create an array
-            char[] chars = createArray();
-
-            // Display the array
-            System.out.println("The lowercase letters are:");
-            displayArray(chars);
-
-            // Count the occurrences of each letter
-            int[] counts = countLetters(chars);
-
-            // Display counts
-            System.out.println();
-            System.out.println("The occurrences of each letter are:");
-            displayCounts(counts);
 
 
     }
